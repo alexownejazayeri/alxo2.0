b34521ad242d0906afbbfa7c818f368333dbd174
Alex Ownejazayeri <alirezaownejazayeri@gmail.com>
2023-12-22 09:32:29 -0800

    make contact page responsive for desktop and tablets

--- app/pages/Contact/index.ts
+++ app/pages/Contact/index.ts
@@ -1,6 +1,9 @@
+import GSAP from 'gsap'
+
 import Page from '../../classes/Page'
 
 export default class Contact extends Page {
+  hello: any
   constructor() {
     super({
       selector: '.contact',
@@ -11,5 +14,14 @@ export default class Contact extends Page {
     this.createContact()
   }
 
-  createContact() {}
+  createContact() {
+    this.hello = document.querySelector('.contact__heading')?.children[2]
+
+    GSAP.from(this.hello, {
+      duration: 1,
+      x: 500,
+      opacity: 0,
+      textTransform: 'none',
+    })
+  }
 }
